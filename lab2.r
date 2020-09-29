e3ed279cab73049a284b76486a4f97a01302e312
priyanka-cse <53851643+priyanka-cse@users.noreply.github.com>
2020-09-29 12:43:20 +0530

    Delete lab2.r

--- lab2.r
+++
@@ -1,9 +0,0 @@
-Bufftail<-c(10,1,37,5,12)
-Gardenbee<-c(8,3,9,6,4)
-RedTail<-c(18,9,12,4,6)
-CarderBee<-c(8,27,6,32,23)
-HoneyBee<-c(12,13,16,9,10)
-beeFrame<-data.frame(Bufftail,Gardenbee,RedTail,CarderBee,HoneyBee)
-names<-c("Thirstle","vipers","GoldenRain","Yellowafala","blackberry")
-rownames(beeFrame)<-names
-beeFrame
