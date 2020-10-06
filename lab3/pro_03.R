4b41106379ff7b0f69c21774b6ce86317585c2eb
priyanka-cse <53851643+priyanka-cse@users.noreply.github.com>
2020-10-06 11:52:28 +0530

    Add files via upload

---
+++ lab3/pro_03.R
@@ -0,0 +1,8 @@
+MatrixBee<-matrix(data=c(10,1,37,5,12,8,3,9,6,4,18,9,12,4,6,8,27,6,32,23,12,13,16,9,10),nrow=5,ncol=5)
+MatrixBee
+plantnames<-list("Thistle", "vipers", "GoldenRain", "Yellow Alfalfa","blackberry")
+plantnames
+plantmatrix<-as.matrix(plantnames)
+plantmatrix
+rownames(MatrixBee)<-plantmatrix
+MatrixBee
